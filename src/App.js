3d02360276111838ef5977fccaa99edb703544b1
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-11-07 11:04:39 +0100

    :memo: Adding code to use the components in the web

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from "./face01.png";
 import "./App.css";
+import Form from "./components/Form/Form";
+import Robot from "./components/Robot/Robot";
 
 function App() {
   return (
-    <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1>MIND BLOWING - Web in construction.</h1>
-    </div>
+    <>
+      <h1>MY ROBOT LIST</h1>
+      <div className="separator" />
+      <Form />
+      <div className="separator" />
+      <ul className="container__robots">
+        <Robot />
+      </ul>
+    </>
   );
 }
 
