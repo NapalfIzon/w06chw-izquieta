5fa11d21a3233be08eba7e25a2f7778d5c307325
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-11-08 02:14:35 +0100

    :memo: Modify code with className in order to styling Robot component

--- src/redux/thunk/robotsThunks.js
+++ src/redux/thunk/robotsThunks.js
@@ -6,6 +6,11 @@ const endpoint = process.env.REACT_APP_API_ENDPOINT;
 export const loadRobotsThunk = () => {
   return async (dispatch) => {
     const { data: robots } = await axios.get(endpoint);
-    dispatch(loadRobotsAction(robots));
+    const robotsData = robots.map((robot) => {
+      const id = robot._id;
+      delete robot._id;
+      return { ...robot, id: id };
+    });
+    dispatch(loadRobotsAction(robotsData));
   };
 };
