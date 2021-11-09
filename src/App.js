fceb2ae1d0173b14c9681cac2c6e57fe5985ccb3
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-11-09 01:36:33 +0100

    :memo: Adding Login component in App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
+import { useEffect } from "react";
 import "./App.css";
 import Form from "./components/Form/Form";
+import Login from "./components/Login/Login";
 import Robot from "./components/Robot/Robot";
+import useUser from "./hooks/useUser";
 
 function App() {
+  const {
+    user: { isAuthenticated },
+  } = useUser();
+
   return (
     <>
-      <h1>MY ROBOT LIST</h1>
-      <div className="separator" />
-      <Form />
-      <div className="separator" />
-      <ul className="container__robots">
-        <Robot />
-      </ul>
+      {isAuthenticated ? (
+        <div className="App">
+          <h1>MY ROBOT LIST</h1>
+          <div className="separator" />
+          <Form />
+          <div className="separator" />
+          <ul className="container__robots">
+            <Robot />
+          </ul>
+        </div>
+      ) : (
+        <Login />
+      )}
     </>
   );
 }
