0edfd5eb455187747b7db6c8f9c95f737a236b73
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-11-09 22:16:06 +0100

    :zap: improve thunks to obtain and add authorization in header request

--- src/redux/thunk/robotsThunks.js
+++ src/redux/thunk/robotsThunks.js
@@ -1,16 +1,21 @@
 import axios from "axios";
 import { loadRobotsAction } from "../actions/actionCreators";
 
-const endpoint = process.env.REACT_APP_API_ENDPOINT;
+const endpoint = process.env.REACT_APP_API_ENDPOINT + "/robots";
 
-export const loadRobotsThunk = () => {
-  return async (dispatch) => {
-    const { data: robots } = await axios.get(endpoint);
-    const robotsData = robots.map((robot) => {
-      const id = robot._id;
-      delete robot._id;
-      return { ...robot, id: id };
-    });
-    dispatch(loadRobotsAction(robotsData));
-  };
+const token = localStorage.getItem(process.env.REACT_APP_LOCALSTORAGE_KEY);
+
+export const loadRobotsThunk = () => async (dispatch) => {
+  const { data: robots } = await axios.get(endpoint, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const robotsData = robots.map((robot) => {
+    const id = robot._id;
+    delete robot._id;
+    return { ...robot, id: id };
+  });
+
+  dispatch(loadRobotsAction(robotsData));
 };
